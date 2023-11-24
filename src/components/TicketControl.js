4d5f25ad8f2c121f398919d52b5356fe7bc9436e
Paul LeTourneau <pletourneau62@gmail.com>
2023-11-24 11:05:07 -0800

    not working

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import NewTicketForm from './NewTicketForm';
-import TicketList from './TicketList';
-import EditTicketForm from './EditTicketForm';
-import TicketDetail from './TicketDetail';
+import React from "react";
+import NewTicketForm from "./NewTicketForm";
+import TicketList from "./TicketList";
+import EditTicketForm from "./EditTicketForm";
+import TicketDetail from "./TicketDetail";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class TicketControl extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      mainTicketList: [],
       selectedTicket: null,
-      editing: false
+      editing: false,
     };
   }
 
@@ -21,77 +21,114 @@ class TicketControl extends React.Component {
       this.setState({
         formVisibleOnPage: false,
         selectedTicket: null,
-        editing: false
+        editing: false,
       });
     } else {
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         formVisibleOnPage: !prevState.formVisibleOnPage,
       }));
     }
-  }
+  };
 
   handleDeletingTicket = (id) => {
-    const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
-    this.setState({
-      mainTicketList: newMainTicketList,
-      selectedTicket: null
-    });
-  }
+    const { dispatch } = this.props;
+    const action = {
+      type: "DELETE_TICKET",
+      id: id,
+    };
+    dispatch(action);
+    this.setState({ selectedTicket: null });
+  };
 
   handleEditClick = () => {
-    this.setState({editing: true});
-  }
+    this.setState({ editing: true });
+  };
 
   handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = this.state.mainTicketList
-      .filter(ticket => ticket.id !== this.state.selectedTicket.id)
-      .concat(ticketToEdit);
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = ticketToEdit;
+    const action = {
+      type: "ADD_TICKET",
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    };
+    dispatch(action);
     this.setState({
-      mainTicketList: editedMainTicketList,
       editing: false,
-      selectedTicket: null
+      selectedTicket: null,
     });
-  }
+  };
 
   handleAddingNewTicketToList = (newTicket) => {
-    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
-    this.setState({mainTicketList: newMainTicketList});
-    this.setState({formVisibleOnPage: false});
-  }
+    const { dispatch } = this.props;
+    const { id, names, location, issue } = newTicket;
+    const action = {
+      type: "ADD_TICKET",
+      id: id,
+      names: names,
+      location: location,
+      issue: issue,
+    };
+    dispatch(action);
+    this.setState({ formVisibleOnPage: false });
+  };
 
   handleChangingSelectedTicket = (id) => {
-    const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
-    this.setState({selectedTicket: selectedTicket});
-  }
+    const selectedTicket = this.props.mainTicketList[id];
+    this.setState({ selectedTicket: selectedTicket });
+  };
 
-  render(){
+  render() {
     let currentlyVisibleState = null;
-    let buttonText = null; 
-    if (this.state.editing ) {      
-      currentlyVisibleState = <EditTicketForm ticket = {this.state.selectedTicket} onEditTicket = {this.handleEditingTicketInList} />
+    let buttonText = null;
+    if (this.state.editing) {
+      currentlyVisibleState = (
+        <EditTicketForm
+          ticket={this.state.selectedTicket}
+          onEditTicket={this.handleEditingTicketInList}
+        />
+      );
       buttonText = "Return to Ticket List";
     } else if (this.state.selectedTicket != null) {
-      currentlyVisibleState = <TicketDetail 
-      ticket={this.state.selectedTicket} 
-      onClickingDelete={this.handleDeletingTicket}
-      onClickingEdit = {this.handleEditClick} />
+      currentlyVisibleState = (
+        <TicketDetail
+          ticket={this.state.selectedTicket}
+          onClickingDelete={this.handleDeletingTicket}
+          onClickingEdit={this.handleEditClick}
+        />
+      );
       buttonText = "Return to Ticket List";
     } else if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}/>;
-      buttonText = "Return to Ticket List"; 
+      currentlyVisibleState = (
+        <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+      );
+      buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList onTicketSelection={this.handleChangingSelectedTicket} ticketList={this.state.mainTicketList} />;
-      buttonText = "Add Ticket"; 
+      currentlyVisibleState = (
+        <TicketList
+          onTicketSelection={this.handleChangingSelectedTicket}
+          ticketList={this.props.mainTicketList}
+        />
+      );
+      buttonText = "Add Ticket";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button> 
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
-
 }
-
+TicketControl.propTypes = {
+  mainTicketList: PropTypes.object,
+};
+const mapStateToProps = (state) => {
+  return {
+    mainTicketList: state,
+  };
+};
+TicketControl = connect(mapStateToProps)(TicketControl);
 export default TicketControl;
-
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import reducer from "./reducers/ticket-list-reducer";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
