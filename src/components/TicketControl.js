93b07586e3f9207eefef94d593e40c528e8f1e0b
Paul LeTourneau <pletourneau62@gmail.com>
2023-11-27 14:27:13 -0800

    add contributors
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -10,7 +10,7 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
+      // formVisibleOnPage: false,
       selectedTicket: null,
       editing: false,
     };
@@ -19,14 +19,16 @@ class TicketControl extends React.Component {
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
-        formVisibleOnPage: false,
+        // formVisibleOnPage: false,
         selectedTicket: null,
         editing: false,
       });
     } else {
-      this.setState((prevState) => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
+      const { dispatch } = this.props;
+      const action = {
+        type: "TOGGLE_FORM",
+      };
+      dispatch(action);
     }
   };
 
@@ -72,7 +74,10 @@ class TicketControl extends React.Component {
       issue: issue,
     };
     dispatch(action);
-    this.setState({ formVisibleOnPage: false });
+    const action2 = {
+      type: "TOGGLE_FORM",
+    };
+    dispatch(action2);
   };
 
   handleChangingSelectedTicket = (id) => {
@@ -100,7 +105,7 @@ class TicketControl extends React.Component {
         />
       );
       buttonText = "Return to Ticket List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = (
         <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       );
@@ -124,10 +129,12 @@ class TicketControl extends React.Component {
 }
 TicketControl.propTypes = {
   mainTicketList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool,
 };
 const mapStateToProps = (state) => {
   return {
-    mainTicketList: state,
+    mainTicketList: state.mainTicketList,
+    formVisibleOnPage: state.formVisibleOnPage,
   };
 };
 TicketControl = connect(mapStateToProps)(TicketControl);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
-import reducer from "./reducers/ticket-list-reducer";
+import { legacy_createStore } from "redux";
+// import reducer from "./reducers/ticket-list-reducer";
 import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
 
-const store = createStore(reducer);
+const store = legacy_createStore(rootReducer);
 
 store.subscribe(() => console.log(store.getState()));
 
